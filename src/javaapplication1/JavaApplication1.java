bf064ee21424f7b3ceadb99d1cd33a1d507fa8dc
Lohana Lema Moreta. <lohana.8@gmail.com>
2017-01-09 10:23:56 -0500

    Aumentando codigo al repositorio de Valentina

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
+        System.out.println("Modificación Lohana");
     }
     
 }
